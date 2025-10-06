e64926aab3a06e4d67a1a1e000b0d5adf8b8d551
Manu-gomes29 <emanugomes2007@gmail.com>
2025-10-06 13:33:40 +0000

    Emanuelly Gomes de Souza modulo 2 manhã

--- app_flask.py
+++ app_flask.py
@@ -1,13 +1,38 @@
-from flask import Flask, request, render_template
+from flask import Flask, request
+app = Flask (__name__)
 
-app = Flask(__name__)
+@app.route('/')
+def olá():
+    return "olá, Flask!"
 
+@app.route('/soma', methods=["GET"])
+def soma ():
+    valor1 = request.args.get(key= "valor1", default=None, type=float)
+    valor2 = request.args.get(key = "valor2", default=None,type=float)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+    return f"Soma = {valor1 + valor2}"
 
-## Continue o código aqui.
+@app.route('/subtrair', methods=["GET"])
+def subtrair():
+    valor1 = request.args.get(key= "valor1", default=None, type=float)
+    valor2 = request.args.get(key = "valor2", default=None,type=float)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    return f"subtrair = {valor1 - valor2}"
+
+@app.route('/multiplicacao', methods=["GET"])
+def multiplicacao():
+    valor1 = request.args.get(key= "valor1", default=None, type=float)
+    valor2 = request.args.get(key = "valor2", default=None,type=float)
+
+    return f"multiplicacao = {valor1 * valor2}"
+
+@app.route('/divisao', methods=["GET"])
+def divisao():
+    valor1 = request.args.get(key= "valor1", default=None, type=float)
+    valor2 = request.args.get(key = "valor2", default=None,type=float)
+
+    return f"divisao = {valor1 / valor2}"
+
+   
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True)
