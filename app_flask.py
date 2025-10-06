5abc05b22e9e96c14486839aca35d39ff6deac1f
rigoni777 <joaorigoni797@gmail.com>
2025-10-06 20:42:40 +0000

    Joao Victor Rigoni Modulo 2, Tarde

--- app_flask.py
+++ app_flask.py
@@ -8,6 +8,16 @@ def index():
     return render_template("index.html")
 
 ## Continue o código aqui.
+@app.route('/soma')
+def soma():
+    valor1 = request.args.get(key="valor1", default=None ,type=float)
+    valor2 = request.args.get (key="valor2" , default=None , type=float)
+    return f"A soma é: {valor1 + valor2}"
+@app.route('/subtrair')
+def subtrair():
+    valor3 =request.args.get(key='valor3', default=None, type= float)
+    valor4 =request.args.get(valor4,0,float)
+    return f"A soma é: {valor3 - valor4}"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app_sqlite.py
+++ app_sqlite.py
@@ -12,3 +12,36 @@ O que o script deve fazer:
 7) Deletar registros
 
 """
+import _sqlite3
+
+## 1 Criar escola.db
+def conexao_banco():
+    conexao =_sqlite3.connect("escola.db")
+    return conexao
+
+## 2 Crair tabela alunos --> seguindo o diagrama
+with conexao_banco() as conexao:
+    conexao.execute("CREATE TABLE IF NOT EXISTS alunos (id INTEGER PRIMARY KEY, nome TEXT, materia TEXT, data_nascimento DATE)")
+
+## 3 Inserção de dados
+with conexao_banco() as conexao:
+    conexao.execute("INSERT INTO alunos (nome,materia,data_nascimento) VALUES ('Daniel sovado', 'ADM', '1850-12-31')")
+
+## 4 Pegando dados 
+with conexao_banco() as conexao:
+    lista_alunos = conexao.execute("SELECT * FROM alunos").fetchall()
+    print(lista_alunos)
+
+## 5 Pegue os dados pelo id
+with conexao_banco() as conexao:
+    pessoa = conexao.execute("SELECT * FROM alunos WHERE id=2").fetchall()
+    print(pessoa)
+
+## 6 Atualizar registros
+with conexao_banco() as banco:
+    conexao.execute("UPDATE alunos SET nome='Rigoni Victor' WHERE id= 1")
+
+## 7 Deletar 
+with conexao_banco() as banco:
+    conexao.execute("DELETE FROM alunos WHERE data_nascimento = '1999-08-27'")
+    
