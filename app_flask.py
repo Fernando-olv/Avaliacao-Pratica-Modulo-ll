75a4ec9233e1087b28e50ff93db3b7d39a54de4a
Dav-ADSFerre <daviantoniodossantosferreira@gmail.com>
2025-10-06 13:30:51 +0000

    David Antônio dos Santos Ferreira Modulo2 Manhã

--- app_flask.py
+++ app_flask.py
@@ -9,5 +9,29 @@ def index():
 
 ## Continue o código aqui.
 
+@app.route("/soma")
+def soma():
+    valor1 = request.args.get(key="valor1", default=None, type=float)
+    valor2 = request.args.get(key="valor2", default=None, type=float)
+    return f"{valor1 + valor2}"
+
+@app.route("/subtrair")
+def subtrair():
+    valor1 = request.args.get(key="valor1", default=None, type=float)
+    valor2 = request.args.get(key="valor2", default=None, type=float)
+    return f"{valor1 - valor2}"
+
+@app.route("/multiplicar")
+def multiplicar():
+    valor1 = request.args.get(key="valor1", default=None, type=float)
+    valor2 = request.args.get(key="valor2", default=None, type=float)
+    return f"{valor1 * valor2}"
+
+@app.route("/dividir")
+def dividir():
+    valor1 = request.args.get(key="valor1", default=None, type=float)
+    valor2 = request.args.get(key="valor2", default=None, type=float)
+    return f"{valor1 / valor2}"
+
 if __name__ == "__main__":
     app.run(debug=True)
