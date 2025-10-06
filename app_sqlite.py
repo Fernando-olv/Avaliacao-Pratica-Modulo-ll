a5802e74791b6087684dde4f3fe9804342ab7e2c
Manu-gomes29 <emanugomes2007@gmail.com>
2025-10-06 14:52:15 +0000

    Emanuelly Gomes  modulo 2 turma manhã

--- app_sqlite.py
+++ app_sqlite.py
@@ -14,6 +14,20 @@ O que o script deve fazer:
 """
 import sqlite3
 
+## 1 - Criação do banco
 conexao = sqlite3.connect("./escola.db")
+# 2 - Criação da tabela
+conexao.execute("CREATE TABLE IF NOT EXISTS alunos (id INTEGER PRIMARY KEY,nome TEXT,idade INTERGER,email TEXT)")
+# 3 - Inserção de dados
+conexao.execute("INSERT INTO alunos (nome, idade, email) VALUES ('LEONARDO',20, 'leonardo@gmail.com')"),
+conexao.commit()
+#4 - Seleção dos dados
+print(conexao.execute("select * FROM alunos").fetchall())
+#5 - Update
+conexao.execute ("UPDATE alunos SET email = 'superemail@gmail.com' WHERE id=1")
+conexao.commit()
 
-conexao.execute("CREATE TABLE alunos (id INTEGER PRIMARY KEY,nome TEXT,idade)")
+print(conexao.execute("select * FROM alunos").fetchall())
+#7- delete 
+conexao.execute("DELETE FROM alunos WHERE id =1 ")
+
