add4674c1f7082abc90f2e69bd8788c0a45a9502
Matheus De Oliveira <matheusoliveira052429@gmail.com>
2025-10-06 20:43:57 +0000

    Matheus De Oliveira Módulo 2 Tarde

--- app_sqlite.py
+++ app_sqlite.py
@@ -1,14 +1,41 @@
 """
 Avaliação – Python + SQLite
 Tema: CRUD em 'alunos'
-
-O que o script deve fazer:
-1) Criar 'escola.db'
-2) Criar tabela 'alunos' -> Seguindo o diagrama
-3) Inserir registros na tabela alunos
-4) Listar todos
-5) Buscar por id
-6) Atualizar registros
-7) Deletar registros
-
 """
+import sqlite3
+
+#O que o script deve fazer:
+#1) Criar 'escola.db'
+ 
+def conectar_banco():
+    conexao = sqlite3.connect("escola.db")
+    return conexao
+
+#2) Criar tabela 'alunos' -> Seguindo o diagrama
+
+with conectar_banco() as professores:
+    professores.execute("CREATE TABLE IF NOT EXISTS alunos (id INTEGER PRIMARY KEY, nome TEXT NOT NULL, idade INTERGER, email TEXT NOT NULL)")
+    
+#3) Inserir registros na tabela alunos
+
+with conectar_banco() as professores:
+    professores.execute("INSERT INTO alunos (nome, idade, email) VALUES ('Osvaldo', 12, 'omi@gmail.com')")
+
+#4) Listar todos
+with conectar_banco() as professores:
+    professores.execute("SELECT * FROM alunos")
+
+#5) Buscar por id
+
+with conectar_banco() as professores:
+    professores.execute("SELECT * FROM alunos WHERE id=2")
+
+#6) Atualizar registros
+
+with conectar_banco() as professores:
+    professores.execute("UPDATE alunos SET nome='Rubens' WHERE id=1")
+
+#7) Deletar registros
+
+with conectar_banco() as professores:
+    professores.execute('DELETE FROM alunos WHERE id=1')
