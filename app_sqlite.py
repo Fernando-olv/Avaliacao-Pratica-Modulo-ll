be8780d2d8b5c6e33aac73a0482c68bbfcb595f8
Joao-VMD <joaovictormogliadantas@gmail.com>
2025-10-06 14:30:58 +0000

    João Victor Moglia Dantas Modulo2 manhã

--- app_sqlite.py
+++ app_sqlite.py
@@ -12,3 +12,32 @@ O que o script deve fazer:
 7) Deletar registros
 
 """
+import sqlite3
+
+conexao = sqlite3.connect("./escola.db")
+
+conexao.execute("CREATE TABLE if not exists alunos (id INTEGER PRIMARY KEY, nome TEXT, idade INTEGER, email TEXT NOT NULL UNIQUE)")
+
+conexao.execute("INSERT OR IGNORE INTO alunos(nome,idade,email) values ('João Pereira',18,'joaozinho@gmail.com')")
+
+conexao.commit()
+
+#lista a lista
+mos = conexao.execute("SELECT * FROM alunos").fetchall()
+
+print (mos)
+#busca id
+bus_id = conexao.execute("SELECT * FROM alunos WHERE id= 1 ").fetchall()
+print(bus_id)
+
+#Atualiza
+conexao.execute("UPDATE alunos SET nome='joaozinhooo' WHERE id=1")
+conexao.commit()
+#busca o id atualizado
+bus_id = conexao.execute("SELECT * FROM alunos WHERE id= 1 ").fetchall()
+print(bus_id)
+#deleta
+conexao.execute("DELETE FROM alunos WHERE id=1  ")
+conexao.commit()
+
+
