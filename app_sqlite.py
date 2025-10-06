9b66ceb2b54df73fefad795217cf5179158a46da
Leo-Borba1 <leonardo.ferborba@gmail.com>
2025-10-06 14:34:29 +0000

    Leonardo Ferreira Borba   Modulo 2 Manhã

--- app_sqlite.py
+++ app_sqlite.py
@@ -12,3 +12,22 @@ O que o script deve fazer:
 7) Deletar registros
 
 """
+
+import sqlite3
+
+conexao = sqlite3.connect('./escola.db')
+
+conexao.execute('CREATE TABLE IF NOT EXISTS alunos (id INTEGER PRIMARY KEY, nome TEXT, idade INTEGER, email TEXT)')
+
+conexao.execute("INSERT INTO alunos(nome, idade, email) VALUES ('Kleber', 61, 'klaber.011@gmail.com')")
+conexao.commit()
+print(conexao.execute("SELECT *  FROM alunos").fetchall())
+
+print(conexao.execute('SELECT * FROM alunos WHERE id= 1'))
+
+##6 Atualizar Registro
+conexao.execute("UPDATE alunos SET nome='Jorge' WHERE id=1")
+conexao.commit()
+
+conexao.execute("DELETE FROM alunos WHERE id=1")
+conexao.commit()
